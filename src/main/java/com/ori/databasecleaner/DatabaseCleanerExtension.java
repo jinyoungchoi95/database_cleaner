2829d588f7bf3998f38696544dec1ac28d27777d
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-15 13:42:38 +0900

    final

--- src/main/java/com/ori/databasecleaner/DatabaseCleanerExtension.java
+++ src/main/java/com/ori/databasecleaner/DatabaseCleanerExtension.java
@@ -1,9 +1,9 @@
 package com.ori.databasecleaner;
 
+import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Objects;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -18,42 +18,25 @@ public class DatabaseCleanerExtension implements AfterEachCallback {
     @Transactional
     public void afterEach(final ExtensionContext context) {
         ApplicationContext applicationContext = SpringExtension.getApplicationContext(context);
-        try {
-            System.out.println(1);
-            JdbcTemplate jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
-            System.out.println(2);
-            DatabaseMetaData metaData = Objects.requireNonNull(jdbcTemplate.getDataSource())
-                    .getConnection()
-                    .getMetaData();
-            System.out.println(3);
-            String[] types = {"TABLE"};
-            ResultSet rs = metaData.getTables(null, null, "%", types);
+        JdbcTemplate jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
+        try (Connection connection = Objects.requireNonNull(jdbcTemplate.getDataSource()).getConnection()) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            ResultSet rs = metaData.getTables(null, null, "%", new String[]{"TABLE"});
 
-            System.out.println(4);
             executeResetTableQuery(jdbcTemplate, rs);
-            System.out.println(5);
         } catch (Exception exception) {
-            System.out.println(6);
-            System.out.println(exception.getMessage());
-            System.out.println(7);
-            throw new RuntimeException();
+            throw new RuntimeException("database table clean error");
         }
     }
 
     private void executeResetTableQuery(final JdbcTemplate jdbcTemplate, final ResultSet rs) throws SQLException {
-        System.out.println(1);
         jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY FALSE");
-        System.out.println(2);
         while (rs.next()) {
-            System.out.println(11);
             String tableName = rs.getString("TABLE_NAME");
             jdbcTemplate.execute(createTruncateTableQuery(tableName));
-            System.out.println(22);
             jdbcTemplate.execute(createResetAutoIncrementQuery(tableName));
-            System.out.println(33);
         }
         jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY TRUE");
-        System.out.println(4);
     }
 
     private String createTruncateTableQuery(final String tableName) {
